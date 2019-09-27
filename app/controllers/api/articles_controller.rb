14b6958ef5c18e6d174e06a2955a235c84222534
jshin1223 <jshin1223@gmail.com>
2019-09-27 11:09:26 -0500

    initial commit

---
+++ app/controllers/api/articles_controller.rb
@@ -0,0 +1,11 @@
+class Api::ArticlesController < ApplicationController
+
+  def index
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ ENV['API_KEY'] }")
+
+    @articles = response.parse["articles"]
+    render json: @articles
+
+  end
+
+end
